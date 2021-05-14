2bb5057210fcb8240e4b568acc593c86a88603d9
nehaKumari33 <77428717+Nehakumari33@users.noreply.github.com>
2021-05-14 23:14:42 +0530

    Updates

---
+++ precache-manifest.9c92eff8cd7b9f0f94f9947c5ca9db30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af084cd9a01892c8414701b09e89084",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c6c5192099f93d519",
+    "url": "/static/css/2.8163d445.chunk.css"
+  },
+  {
+    "revision": "64a0972efc7f5891903f",
+    "url": "/static/css/main.c11fd00e.chunk.css"
+  },
+  {
+    "revision": "939c6c5192099f93d519",
+    "url": "/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a0972efc7f5891903f",
+    "url": "/static/js/main.a8fd63cf.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
